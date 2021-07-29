0a65588de4466c25842c4eaac272e340a3693408
jaychy-yy <0816jinjin@gmail.com>
2021-07-29 09:48:53 +0900

    feat: log4j2 기본 설정 완료

---
+++ src/test/java/com/j/log/SpringStarterLog4j2LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterLog4j2LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
